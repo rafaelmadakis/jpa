10b2d6c37559f46f4298b070120309079377bda3
rafaelmadakis <rafaelmadakis@gmail.com>
2020-05-05 22:53:33 -0300

    Entendendo e configurando JUnit

---
+++ src/test/java/com/algaworks/junit/EntendendoJunitTest.java
@@ -0,0 +1,43 @@
+package com.algaworks.junit;
+
+import org.junit.*;
+
+public class EntendendoJunitTest {
+
+    @BeforeClass
+    public static void iniciarTestes(){
+        System.out.println(" >>>>> public static void iniciarTestes() <<<<");
+    }
+
+    @AfterClass
+    public static void encerrarTestes(){
+        System.out.println(" >>>>> public static void encerrarTestes() <<<<");
+    }
+
+    @Before
+    public void iniciarTeste() {
+        System.out.println(" >>>>> public void iniciarTeste() <<<<");
+    }
+
+    @After
+    public void encerrarTeste() {
+        System.out.println(" >>>>> public void encerrarTeste() <<<<");
+    }
+
+
+    @Test
+    public void testandoAlgo(){
+        String nome = String.format("%s", "Alexandre");
+
+        Assert.assertEquals("Alexandre", nome);
+    }
+
+    @Test
+    public void testandoOutraCoisa(){
+
+        String srt = String.format("%s", "");
+
+        Assert.assertTrue(srt.isEmpty());
+    }
+
+}
